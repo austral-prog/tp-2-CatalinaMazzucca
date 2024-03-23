7e7c6aa972d3a2f7afe2b04a4f70ef671e71d979
CatalinaMazzucca <162723477+CatalinaMazzucca@users.noreply.github.com>
2024-03-23 18:54:47 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,5 +1,8 @@
 def earth():
     x: "Bangladesh" 
     y: "Barbados" 
-    print("Bangladesh" < "Barbados")
-    print("Bangladesh" > "Barbados")
+    a=print("Bangladesh" < "Barbados")
+    b=print("Bangladesh" > "Barbados")
+    print(f"The result of Bangladesh comes first in the dictionary than Barbados is {a}.")
+    print(f"The result of Barbados comes first in the dictionary than Bangladesh is {b}.")
+    
