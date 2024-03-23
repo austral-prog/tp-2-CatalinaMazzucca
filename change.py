932b4e566b5d2aa695a09f325c4ec00152bb999d
CatalinaMazzucca <162723477+CatalinaMazzucca@users.noreply.github.com>
2024-03-23 12:45:58 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,7 +1,7 @@
 def change():
     expense = 23.75
     money = 100
-    print(int (money - expense))
+    print(int(money-expense))
     76
-    print(float (money - expense) %1)
+    print(float(money-expense) %1)
     0.25
