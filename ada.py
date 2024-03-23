5abfaba6456b547376595cb377735230f746ab7e
CatalinaMazzucca <162723477+CatalinaMazzucca@users.noreply.github.com>
2024-03-23 18:43:25 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,7 +1,8 @@
 def ada():
-    first_name = "AdA"
+    first_name = "    AdA"
     last_name = "LoVeLAce"
     full_name = "    AdA LoVeLace"
     print(full_name.lower())
     print(full_name.title())
     print(full_name.upper())
+    print(full_name.lower().rstrip())
